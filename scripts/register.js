9598db4d1bb3206b417197eb3f81556503157595
roopam527 <roopamg777@gmail.com>
2018-10-02 17:53:07 +0530

    handling errors

--- scripts/register.js
+++ scripts/register.js
@@ -5,6 +5,11 @@ sessionStorage.setItem("logged_in_user",JSON.stringify(data))
     console.log(sessionStorage.getItem("logged_in_user"))
 }
 
+const error = document.getElementById("error");
+const remove_error = () =>{
+   error.remove();
+}
+
 const register = () =>{
     
     const email = document.getElementById("email").value;
@@ -36,6 +41,9 @@ const register = () =>{
 
             console.log(data);
             if(data){
+                if(error){
+                    remove_error()
+                }
                 var d1 = document.getElementById('email');
             d1.insertAdjacentHTML('afterend', `<div id="error">Ooops! User Already Exist</div>`);
             return 0;
@@ -44,14 +52,21 @@ const register = () =>{
             
         })
         .catch((err)=>{
+            if(error){
+                remove_error()
+            }
             var d1 = document.getElementById('logo');
             d1.insertAdjacentHTML('afterend', `<div id="error">Slow Internet Connection</div>`);
            
         })
     }
     else{
+        if(error){
+            remove_error()
+        }
         if(!valid_password)
         {
+            
             var d1 = document.getElementById('password');
             d1.insertAdjacentHTML('afterend', `<div id="error">Password must have: <br/> One special character and <br/> Should be alphanumeric</div>`);
         }
